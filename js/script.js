1e8f14409603b889994a7ee22470a842ccc373c7
yuki <y.matsu880077@gmail.com>
2025-10-10 08:57:21 +0900

    script.js を更新

--- js/script.js
+++ js/script.js
@@ -136,32 +136,7 @@ particlesJS("particles-js-canvas-el",
         "move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
         "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 
-/*
-画像ぎゃらり
-================================================ */
 
-document.addEventListener('DOMContentLoaded', function () {
-    const mainImg = document.getElementById('main-display-img');
-    const mainText = document.getElementById('main-display-text');
-    const thumbs = document.querySelectorAll('.gallery-thumbnails img');
-
-  // 初期表示（gallary1.jpgとそのテキストを表示）
-  if (thumbs.length > 0) {
-    mainImg.src = thumbs[0].src;
-    mainImg.style.display = 'block';
-    mainText.textContent = thumbs[0].getAttribute('data-text');
-    mainText.style.display = 'block';
-  }
-
-  // サムネイルとテキストは常に表示
-  thumbs.forEach(thumb => {
-    thumb.addEventListener('mouseenter', function () {
-      mainImg.src = this.src;
-      mainText.textContent = this.getAttribute('data-text');
-    });
-    // マウスが離れてもmain画像・テキストは消さない
-  });
-});
 
 particlesJS("particles-ga", 
   {"particles":
