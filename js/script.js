2349fe85fdd634b33708c4dd6e08e3e7d28d4bb6
yuki <y.matsu880077@gmail.com>
2025-10-10 09:07:45 +0900

    script.js を更新

--- js/script.js
+++ js/script.js
@@ -1,3 +1,74 @@
+
+const loadingAreaGrey = document.querySelector('#loading');
+const loadingAreaGreen = document.querySelector('#loading-screen');
+const loadingText = document.querySelector('#loading p');
+
+window.addEventListener('load', () => {
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(
+    {
+      opacity: [1, 0],
+      visibility: 'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh']
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );  
+
+  // ローディング中テキスト
+  loadingText.animate(
+    [
+      {
+        opacity: 1,
+        offset: .8  //80%
+      },
+      {
+        opacity: 0,
+        offset: 1  //100%
+      },
+    ], 
+    {
+      duration: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
+
+particlesJS("particles-js-canvas-el", 
+  {"particles":
+    {"number":
+      {"value":200,
+        "density":{"enable":true,"value_area":800}},
+        "color":{"value":"#ffffff"},
+        "shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
+        "opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
+        "size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
+        "line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
+        "move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
+        "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+
+
+
+
+
+
 // ページめくり（本をめくるようなイメージ）
 document.addEventListener('DOMContentLoaded', function () {
   const items = document.querySelectorAll('.text-item');
@@ -67,76 +138,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
-/*
-
-ローディングから画面遷移
-================================================ */
-const loadingAreaGrey = document.querySelector('#loading');
-const loadingAreaGreen = document.querySelector('#loading-screen');
-const loadingText = document.querySelector('#loading p');
-
-window.addEventListener('load', () => {
-  // ローディング中（グレースクリーン）
-  loadingAreaGrey.animate(
-    {
-      opacity: [1, 0],
-      visibility: 'hidden',
-    },
-    {
-      duration: 2000,
-      delay: 1200,
-      easing: 'ease',
-      fill: 'forwards',
-    }
-  );
-
-  // ローディング中（薄緑スクリーン）
-  loadingAreaGreen.animate(
-    {
-      translate: ['0 100vh', '0 0', '0 -100vh']
-    },
-    {
-      duration: 2000,
-      delay: 800,
-      easing: 'ease',
-      fill: 'forwards',
-    }
-  );  
-
-  // ローディング中テキスト
-  loadingText.animate(
-    [
-      {
-        opacity: 1,
-        offset: .8  //80%
-      },
-      {
-        opacity: 0,
-        offset: 1  //100%
-      },
-    ], 
-    {
-      duration: 1200,
-      easing: 'ease',
-      fill: 'forwards',
-    }
-  );
-});
-
-particlesJS("particles-js-canvas-el", 
-  {"particles":
-    {"number":
-      {"value":200,
-        "density":{"enable":true,"value_area":800}},
-        "color":{"value":"#ffffff"},
-        "shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},
-        "opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},
-        "size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
-        "line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
-        "move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
-        "interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
-
-
 
 
 
